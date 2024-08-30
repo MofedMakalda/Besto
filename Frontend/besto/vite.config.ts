87c43c65acf1c7e0bf73c8176e0919fe17e9138e
mofedmakalda <mofedmk1@gmail.com>
2024-08-30 03:41:38 +0300

    new updates

--- Frontend/besto/vite.config.ts
+++ Frontend/besto/vite.config.ts
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0', // Allow access from any IP
-    port: 3000, // Your desired port
+  // server: {
+  //   host: '0.0.0.0', // Allow access from any IP
+  //   port: 3000, // Your desired port
   },
-});
+);
