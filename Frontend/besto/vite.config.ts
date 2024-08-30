e7f217e4e804056a6eeb15711cfa2017c49adaaf
mofedmakalda <mofedmk1@gmail.com>
2024-08-30 03:46:32 +0300

    somechanges.

--- Frontend/besto/vite.config.ts
+++ Frontend/besto/vite.config.ts
@@ -1,12 +1,11 @@
-// vite.config.ts
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   host: '0.0.0.0', // Allow access from any IP
-  //   port: 3000, // Your desired port
+  base: '/', // This line can help if you're deploying to a subpath
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
   },
-);
+});
